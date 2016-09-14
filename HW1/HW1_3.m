2d4fe57cb699f11025a85aae58612ed7c015ec90
Zhangliang <zhangliang@ZhangliangdeMacBook.local>
2016-09-14 15:46:09 -0700

    Fix some mistakes in the HW1_3 about the bootstrap method.

--- HW1/HW1_3.m
+++ HW1/HW1_3.m
@@ -1,9 +1,11 @@
 load NJGAS.dat;
 resample_size=length(NJGAS);
+sample_mean=sum(NJGAS)/resample_size;
 resample_time=1000;
-index=randi([1 12],resample_time,resample_size);
+index=randi([1 resample_size],resample_time,resample_size);
 resampled_data=NJGAS(index);
-erd=sum(resampled_data,2)/resample_size;
+erd=sum(resampled_data,2)/resample_size-sample_mean;
 erd_sorted=sort(erd);
-int=erd_sorted(fix([0.25 0.975]*resample_time));
+delta=erd_sorted(fix([0.025 0.975]*resample_time));
+int=sample_mean-[delta(2) delta(1)];
 disp(strcat('The 95% interval is [',num2str(int(1)),', ',num2str(int(2)),']'));
