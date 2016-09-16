58d2ec29eadf1bd7e6d1d4091a7465e34c1f9bf6
Zhangliang <zhangliang@ZhangliangdeMacBook.local>
2016-09-15 23:20:29 -0700

    Fix mistakes in the 1.2 that the sample should come from the 5 Bernoulli trials

--- HW1/HW1_2.m
+++ HW1/HW1_2.m
@@ -1,31 +1,38 @@
-trial_time=300;
-i=1;
+trial_time = 3000;
+i = 1;
+SAMPLE_MEAN = 2.5;
+SAMPLE_VAR = 1.25;
 for k=[2 5 10 30 50]
     %%
-    %This section generates 300 such sums and histogram the results.
-    trials=randi([0 1],k,trial_time);
-    samples=sum(trials);
+    % This section generates 300 such sums and histogram the results.
+    trials = randi([0 1], k, trial_time, 5); % Generate the 5 successes Bernoulli trial.
+    trials = sum(trials,3);
+    samples = sum(trials);
     subplot(5,1,i);
-    i=i+1;
-    hist(samples,min(samples)-1:max(samples)+1);
+    i = i + 1;
+    [counts, centers] = hist(samples,(min(samples)-1):(max(samples)+1));
+    bar(centers, counts);
     %%
-    %This section plot the expected frequency from the Gaussian
+    % This section plot the expected frequency from the Gaussian
     %distribution as a comparison to the result.
     hold on
-    scaler=normcdf(0.5/(sqrt(k)*0.5))-normcdf(-0.5/(sqrt(k)*0.5));
-    plot_range=min(samples)-2:0.1:max(samples)+2;
-    y = gaussmf(plot_range,[sqrt(k)*0.5 0.5*k])*scaler*trial_time;
+    plot_range = (min(samples)-2):0.1:(max(samples)+2);
+    y = pdf('Normal', plot_range, SAMPLE_MEAN*k, sqrt(k)*SAMPLE_VAR) * trial_time;
     plot(plot_range,y,'r');
-    points=min(samples)-1-0.5:1:max(samples)+1+0.5;
-    points=points-k*0.5;
-    points=points/(sqrt(k)*0.5);
-    pp=normcdf(points);
-    ppd=pp(2:end)-pp(1:end-1);
-    bar(min(samples)-1:max(samples)+1,ppd*trial_time,'stacked','w');
+    points = min(samples)-1-0.5:1:max(samples)+1+0.5;
+    points = points-k*SAMPLE_MEAN;
+    points = points/(sqrt(k)*SAMPLE_VAR);
+    pp = normcdf(points);
+    ppd = pp(2:end)-pp(1:end-1);
+    bar(min(samples)-1:max(samples)+1,ppd * trial_time,0.5,'w');
     %%
     xlabel(strcat('sum(k=',num2str(k),')'));
     ylabel('num');
     hold off
+    %% Data difference 
+    % diff=sum(abs(ppd*trial_time-counts));
+    % disp('The difference is:')
+    % disp(diff);
 end
 
 
